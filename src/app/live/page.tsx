2abd4aef19bb70c091a0a7fc3f98518e88c0102a
Mahdi Malverdi <mahdi.malverdi@gmail.com>
2024-07-17 21:11:18 +0330

    remove params from live

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,9 +1,9 @@
-import {Params} from "next/dist/shared/lib/router/utils/route-matcher";
+
 import Header from "@/app/_components/header";
 import React from "react";
 import {Live} from "@/app/_components/live";
 
-export default async function LivePage({ params }: Params) {
+export default async function LivePage() {
     return (
         <main>
             <Header />
